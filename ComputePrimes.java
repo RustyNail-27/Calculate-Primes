3367c0a9f757a5ab333fed87251f7cd7a62fc149
RustyNail-27 <russell.d.g.walton@gmail.com>
2016-02-22 18:22:45 -0800

    compute-prime-java
    Completed java code to compute primes given an input n

---
+++ ComputePrimes.java
@@ -0,0 +1,110 @@
+/*
+ * Russell Walton
+ * T00249622
+ * COMP 3050
+ * Assignment 1
+ * 
+ * Purpose: to develop an efficient algorithm to compute and print all prime numbers between 2 and n (inclusive).
+ * 
+ * Language: Java
+ * Editor/Compiler: Eclipse
+ * OS: Mac OSX 10.11.2
+ * Computer: MacBook Pro 2.9 GHz Intel Core i7
+ * 
+ * 
+ */
+
+import java.util.Scanner;
+
+
+public class ComputePrimes {
+
+	public static void main(String[] args) {
+		int max, primeCount, primesIndex, topPrimes;
+		boolean[] primeCheck;
+		int[] allInts, primeInts;
+		String primeList;
+		long startTime, endTime;
+		
+		Scanner input = new Scanner(System.in);
+		
+		//take user input for the max value
+		System.out.println("Calculate all primes up to: ");
+		while (!input.hasNextInt()) 
+			input.next();
+		max = input.nextInt();
+		
+		//computation time start point
+		startTime = System.nanoTime();
+		
+		primeCheck = new boolean[max + 1];
+		primeCount = 0;
+		primesIndex = 0;
+		primeList = "";
+		allInts = new int[max-2];
+	    	for(int i = 0; i<allInts.length;i++)
+	    	{
+	    		allInts[i] = (i+2);
+	    	}
+	    primeInts = new int[max];
+		
+
+    	
+        for (int i = 2; i <= max; i++) 
+        {
+            primeCheck[i] = true;
+        }
+
+        //set non-prime indexes of primeCheck matrix to false
+        for (int i = 2; i*i <= max; i++) 
+        {
+            if (primeCheck[i]) 
+            {
+                for (int j = i; j*i <= max; j++) 
+                {
+                    primeCheck[j*i] = false;
+                }
+            }
+        }
+
+        
+        // populates primeInts matrix with the primes up to input max & counts them
+        for(int i = 0; i<=max-2;i++)
+        {
+        	if(primeCheck[i+2])
+        	{
+        		primeInts[primesIndex] = allInts[i];
+        		primesIndex++;
+        		primeCount++;
+        	}
+        }
+        
+        // adds the top 50 prime numbers to the primeList String for output
+        topPrimes = 50;
+        for(int i = primeInts.length-1;i>=0;i--)
+        {	
+        	if(topPrimes > 0)
+        	{
+	        	if(primeInts[i] != 0)
+	        	{
+	        		primeList += primeInts[i] +" ";
+	        		topPrimes--;
+	        		if(topPrimes == 25)
+	        			primeList += "\n";
+	        	}
+        	}
+        }
+        
+        //computation time end point
+        endTime = System.nanoTime();
+        
+        System.out.println("Given n = " + max);
+        System.out.println("Numbers of primes is: " + primeCount);
+        System.out.println("Top 50 prime numbers: ");
+        System.out.println(primeList);
+        System.out.println("Elapsed time: " + (endTime - startTime) + " nanoseconds");
+    }
+
+}
+
+
